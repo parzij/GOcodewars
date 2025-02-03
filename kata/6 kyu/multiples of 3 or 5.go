f61da4fb272efae86e138ed191058bd00a83aaee
Vyacheslav Batychkov <128066686+parzij@users.noreply.github.com>
2025-02-03 11:47:07 +0300

    Create multiples of 3 or 5.go

---
+++ kata/6 kyu/multiples of 3 or 5.go
@@ -0,0 +1,12 @@
+package kata
+
+func Multiple3And5(number int) int {
+  sum := 0
+  for i := 0; i < number; i++ {
+    if i % 3 == 0 || i % 5 == 0 {
+      sum += i
+    }
+  } 
+  return sum
+  
+}
